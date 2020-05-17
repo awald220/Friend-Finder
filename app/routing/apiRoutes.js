3af1df050f5add40afc89b008d1ef253077245f3
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-17 09:00:19 -0500

    add post route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,13 @@
 var friends = require("../data/friends");
 
 module.exports = function(app){
+
+    // get request
     app.get("/api/friends", function(req, res){
         res.json(friends);
     });
+
+    app.post("/api/friends", function(req, res){
+        res.json(friends);
+    })
 }
