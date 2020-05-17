6722d414eac60636feff519ae26df1d04f2ea3bd
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-17 13:55:22 -0500

    add server connection

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
-var express = require("express")
-var path = require("path")
+//Dependencies:
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+//ROUTER
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
