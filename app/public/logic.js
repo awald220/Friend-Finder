e89abca03e9873b53f333529978c4918371774b8
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-17 09:21:10 -0500

    create on click function to get the values of the usersinput

---
+++ app/public/logic.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+$("#submit").on(click, function(){
+    event.preventDefault();
+
+    var newFriend = {
+        name: $("#name").val().trim(),
+        photo: $("#photo").val().trim(),
+        scores: [
+            $("#question-1").val().trim(),
+            $("#question-2").val().trim(),
+            $("#question-3").val().trim(),
+            $("#question-4").val().trim(),
+            $("#question-5").val().trim(),
+            $("#question-6").val().trim(),
+            $("#question-7").val().trim(),
+            $("#question-8").val().trim(),
+            $("#question-9").val().trim(),
+            $("#question-10").val().trim()
+        ]
+    };
+
+    // create the new friend in the api/friends page
+    $.post("/api/friends", newFriend)
+
+    
+});
+});
