2fd7e7e00ac028248054cae4f3160bbea939be4f
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-17 13:55:30 -0500

    add friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,39 @@
-var friendsArr = [
-    {
-        name: 'Ashley',
-        photo: "", 
-        scores: [
-            
-        ]
 
-    }
-]
+// Data structure that will store the friends data
+var friendsArr = [
+	
+	{
+		name: 'Kermit the Frog',
+		photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+		scores: [
+			'5',
+			'2',
+			'4',
+			'5',
+			'3',
+			'2',
+			'1',
+			'3',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'1',
+			'2',
+			'1',
+			'5',
+			'3',
+			'3',
+			'4',
+			'2',
+			'5',
+			'4'
+		]
+	}
+];
+
+module.exports = friendsArr;
