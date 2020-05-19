de58d961cb70f300e381613793b03c78677e30d0
Ashley Waldner <ashleywaldner22@gmail.com>
2020-05-19 14:14:40 -0500

    add more example friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,31 +6,82 @@ var friendsArr = [
 		photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
 		scores: [
 			'5',
-			'2',
-			'4',
 			'5',
-			'3',
-			'2',
-			'1',
-			'3',
-			'4',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
 			'5'
 		]
 	},
 	{
-		name: 'Elmo',
-		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		name: 'Scooter',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+		scores: [
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4'
+		]
+	},
+	{
+		
+		name: "Winnie the Pooh",
+		photo: "https://pbs.twimg.com/profile_images/691863315853762561/ClkLQse3.jpg",
+		scores:[
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+
+		]
+	},
+	{
+		
+		name: 'Mrs. Piggy',
+		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		scores: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2'
+		]
+	},
+	{
+		name: 'Spencer',
+		photo: 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
 		scores: [
 			'1',
-			'2',
 			'1',
-			'5',
-			'3',
-			'3',
-			'4',
-			'2',
-			'5',
-			'4'
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
 		]
 	}
 ];
